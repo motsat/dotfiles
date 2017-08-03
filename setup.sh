8b7e36dbedb87f047fa4554ce0e7914827b0bf47
motsat <konpeiex@gmail.com>
2017-08-04 00:45:12 +0900

    update

--- setup.sh
+++ setup.sh
@@ -5,3 +5,7 @@ echo $HOME
 ln -sf {$PWD,$HOME}/.zshrc
 ln -sf {$PWD,$HOME}/.vimrc
 ln -sf {$PWD,$HOME}/.gitconfig
+
+# for dein.vim
+# https://github.com/Shougo/dein.vim
+mkdir -p $HOME/.vim/bundles
