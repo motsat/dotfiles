70e7ae1a0345ac83438eebed9aff2e32c3d6910d
motsat <konpeiex@gmail.com>
2018-10-22 09:55:56 +0900

    neovim mkdirが先だった

--- setup.sh
+++ setup.sh
@@ -5,9 +5,11 @@ echo $HOME
 ln -sf {$PWD,$HOME}/.zshrc
 ln -sf {$PWD,$HOME}/.vimrc
 ln -sf {$PWD,$HOME}/.gitconfig
-ln -sf $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
-
 # for dein.vim
 # https://github.com/Shougo/dein.vim
 mkdir -p $HOME/.vim/bundles
+
+# for neovim
 mkdir -p $HOME/.config/nvim
+ln -sf $HOME/dotfiles/nvim/init.vim $HOME/.config/nvim/init.vim
+
